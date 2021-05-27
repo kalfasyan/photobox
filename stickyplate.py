2d7d967a62bfde906a60de78d3867c134ad42501
Yannis <kalfasyan@gmail.com>
2021-05-27 15:21:38 +0200

    added colour correction

--- stickyplate.py
+++ stickyplate.py
@@ -137,6 +137,39 @@ class StickyPlate(object):
             self.undistorted_img = dst
             self.pil_undistorted_img = Image.fromarray(dst)
 
+    def colorcorrect(self, plot=False):
+        import colour
+        import matplotlib.pyplot as plt
+        from colour_checker_detection import (colour_checkers_coordinates_segmentation,
+                                            detect_colour_checkers_segmentation)
+        from colour_checker_detection.detection.segmentation import adjust_image
+
+        colour_checker_image_path = [f"{str(self.chessboard_dir)}/color_calib_plate.png"]
+        colour_checker_imgs = [colour.cctf_decoding(colour.io.read_image(path)) for path in colour_checker_image_path]
+
+        # Detection
+        SWATCHES = []
+        for image in colour_checker_imgs:
+            for swatches, colour_checker, masks in detect_colour_checkers_segmentation(
+                image, additional_data=True):
+                SWATCHES.append(swatches)
+
+        # Colour Fitting
+        D65 = colour.ILLUMINANTS['CIE 1931 2 Degree Standard Observer']['D65']
+        reference_colour_checker = colour.COLOURCHECKERS['ColorChecker 2005']
+        reference_swatches = colour.XYZ_to_RGB(
+                colour.xyY_to_XYZ(list(reference_colour_checker.data.values())),
+                reference_colour_checker.illuminant, D65,
+                colour.RGB_COLOURSPACES['sRGB'].XYZ_to_RGB_matrix)
+
+        # Custom image colour correction
+        img = colour.cctf_decoding(colour.io.read_image(self.path))
+        self.calibrated = colour.cctf_encoding(colour.colour_correction(img, SWATCHES[0], reference_swatches))
+        if plot:
+            colour.plotting.plot_image(self.calibrated)
+
+        self.pil_calibrated = Image.fromarray((self.calibrated * 255).astype(np.uint8))
+
     def crop_image(self, height_pxls=100, width_pxls=120):
         logger.info("Cropping..")
 
